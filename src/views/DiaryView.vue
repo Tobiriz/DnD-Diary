97d33ab9d126a2f79deb157c7dfc5131154236bf
Tobias <tobias.weinlich@gmx.de>
2023-03-11 14:54:35 +0100

    Added diaryview for routing purposes

---
+++ src/views/DiaryView.vue
@@ -0,0 +1,37 @@
+<script>
+import { RouterView } from 'vue-router'
+import { useCharacterStore } from '../stores/CharacterStore'
+import { useCampaignStore } from '../stores/CampaignStore'
+import MainNavigation from '../components/MainNavigation.vue'
+
+export default {
+    name: 'Diary',
+
+    components: {
+        RouterView,
+        MainNavigation
+    },
+
+    setup() {
+        const characterStore = useCharacterStore()
+        const campaignStore = useCampaignStore()
+        return {
+            characterStore,
+            campaignStore
+        }
+    },
+
+    mounted() {
+        this.campaignStore.loadCampaignsFromLocalStorage()
+    },
+
+    beforeUnmount() {
+        this.campaignStore.saveCampaginsInLocalStorage()
+    }
+}
+</script>
+
+<template>
+    <MainNavigation />
+    <RouterView />
+</template>
